16635066224f4687a66f4c269ed01b83d91913a3
Dylan0888 <Dylanj55521@outlook.com>
2022-02-01 13:48:59 +0000

    yes

---
+++ hell.py
@@ -0,0 +1,14 @@
+
+#for x in range (1,101):
+#    print(x)
+#print("These are the numbers!")
+
+fours =[ i**4 for i in range (1,13)]
+print (fours)
+
+print ("These are the numbers 1 - 12 to the power of 4" )
+
+
+number = int(input(" pick a number to multiply by 10: "))
+numberOne = number * 10
+print(numberOne)
